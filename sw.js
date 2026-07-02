4a2b5725972f49f7975b33ed3de53ad7f713ee3a
webertmaster <webertsouza23@gmail.com>
2026-07-02 20:25:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // sw.js - Cache dinâmico com Salvo-Conduto para APIs
 // ==========================================
 
-const CACHE_NAME = 'condo-up-v10';
+const CACHE_NAME = 'condo-up-v11';
 
 self.addEventListener('install', event => {
     self.skipWaiting(); // Força a instalação imediata da nova versão
