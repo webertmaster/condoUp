66c7628dedc007ecaa3eeb6373913bb4631b36b8
webertmaster <webertsouza23@gmail.com>
2026-07-03 10:37:02 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,30 @@
+// 1. Importa as ferramentas do Firebase direto do Google
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 
-// As mesmas chaves do seu index.html
-firebase.initializeApp({
+// 2. Suas credenciais do Condo Up
+const firebaseConfig = {
     apiKey: "AIzaSyCY6Jq9GVYJEQrH0JZ9TAWcQVm-cImiwoc",
     authDomain: "portaria-pro-ebc5c.firebaseapp.com",
     projectId: "portaria-pro-ebc5c",
     storageBucket: "portaria-pro-ebc5c.firebasestorage.app",
     messagingSenderId: "948781126590",
     appId: "1:948781126590:web:5e8888eb6e8124f7df5fef"
-});
+};
 
+// 3. Inicia o Firebase no plano de fundo
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Fica escutando as mensagens quando o app estiver em segundo plano (fechado)
+// 4. Lida com as mensagens quando o aplicativo estiver fechado/minimizado
 messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Recebeu mensagem em background: ', payload);
+    console.log('[firebase-messaging-sw.js] Notificação recebida em segundo plano: ', payload);
+    
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/img/icon-512.png.png' // O logo do Condo Up
+        icon: '/img/icon-512.png.png', // O ícone do seu app
+        badge: '/img/icon-512.png.png'
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
