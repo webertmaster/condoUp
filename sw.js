c697bbc1cb4383071a106357da10c5c61a36fd82
Webert <webertsouza23@gmail.com>
2026-07-06 22:09:15 -0300

    Update service worker to version 14

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // sw.js - Cache dinâmico com Salvo-Conduto para APIs
 // ==========================================
 
-const CACHE_NAME = 'condo-up-v13';
+const CACHE_NAME = 'condo-up-v14';
 
 self.addEventListener('install', event => {
     self.skipWaiting(); // Força a instalação imediata da nova versão
