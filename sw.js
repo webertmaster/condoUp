c6c71eda9826107597cd440574e5fabeb2df97dc
Webert <webertsouza23@gmail.com>
2026-07-24 14:51:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -56,7 +56,7 @@ firebase.initializeApp({
 });
 
 // 4. MOTOR DE CACHE (Versão atualizada)
-const CACHE_NAME = 'condo-up-v64';
+const CACHE_NAME = 'condo-up-v66';
 
 self.addEventListener('install', event => {
     self.skipWaiting();
