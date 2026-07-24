5d8e47a341b1cef33eba71610868c43e36a29773
Webert <webertsouza23@gmail.com>
2026-07-24 11:37:34 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -276,7 +276,7 @@ async function ativarNotificacoesPush() {
     }
 
     try {
-        const registration = await navigator.serviceWorker.register('./sw.js?v=122');
+        const registration = await navigator.serviceWorker.register('./sw.js?v=128');
         if (typeof firebase.messaging().useServiceWorker === 'function') {
             firebase.messaging().useServiceWorker(registration);
         }
