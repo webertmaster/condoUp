ac58b7974de43d328b3c51d7ba864a6bcf83b6b7
Webert <webertsouza23@gmail.com>
2026-07-29 13:12:26 -0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+// 1. Importa as ferramentas do Firebase direto do Google
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+// 2. Suas credenciais do Condo Up
+const firebaseConfig = {
+    apiKey: "AIzaSyCY6Jq9GVYJEQrH0JZ9TAWcQVm-cImiwoc",
+    authDomain: "portaria-pro-ebc5c.firebaseapp.com",
+    projectId: "portaria-pro-ebc5c",
+    storageBucket: "portaria-pro-ebc5c.firebasestorage.app",
+    messagingSenderId: "948781126590",
+    appId: "1:948781126590:web:5e8888eb6e8124f7df5fef"
+};
+
+// 3. Inicia o Firebase no plano de fundo
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// 4. Lida com as mensagens quando o aplicativo estiver fechado/minimizado
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Notificação interceptada e tratada pelo Firebase Oficial.', payload);
+    // 🚨 A ordem de "desenhar notificação dupla" foi removida daqui!
+});
