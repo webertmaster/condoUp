0a5287cf697ca55f37df20fae58805373a4c406d
Webert <webertsouza23@gmail.com>
2026-08-07 22:52:52 -0300

    Update service worker for cache and notifications
    Updated cache version and improved notification handling.

--- sw.js
+++ sw.js
@@ -56,7 +56,7 @@ firebase.initializeApp({
 });
 
 // 4. MOTOR DE CACHE (Versão atualizada)
-const CACHE_NAME = 'condo-up-v89';
+const CACHE_NAME = 'condo-up-v90';
 
 self.addEventListener('install', event => {
     self.skipWaiting();
